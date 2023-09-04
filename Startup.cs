3b2b08a90d095b26fcd5f56a79361e4972047b03
kevster01 <90598902+kevster01@users.noreply.github.com>
2023-09-04 00:07:09 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,74 @@
+﻿
+using MinesweeperApp.BusinessServices;
+using MinesweeperApp.DatabaseServices;
+
+namespace MinesweeperApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10000);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+#if DEBUG
+            services.AddTransient<IUserDAO, UserLocalSqlDAO>();
+            services.AddTransient<IGameBoardDAO, GameBoardLocalSqlDAO>();
+#else
+            services.AddTransient<IUserDAO, UserMySqlDAO>();
+            services.AddTransient<IGameBoardDAO, GameBoardMySqlDAO>();
+#endif
+            services.AddTransient<GameboardBusinessService, GameboardBusinessService>();
+            services.AddTransient<SavingLoadingService, SavingLoadingService>();
+            services.AddTransient<LoginBusinessService, LoginBusinessService>();
+            services.AddTransient<RegistrationBusinessService, RegistrationBusinessService>();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseSession();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
